d87d83e223cb2ef6961d2d0e039697cc60df4368
Pi Volt <master_Fatum@hotmail.com>
2018-07-11 16:01:39 +0300

    Добавьте файлы проекта.

---
+++ Domain/Entities/Worker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class Worker
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Фамилия")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Имя")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Отчество")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Адрес электронной почты")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Номер телефона")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Район")]
+        public string Area { get; set; }
+
+        [Required]
+        [Display(Name = "Сфера деятельности")]
+        public string CategoryServises { get; set; }
+
+        [Required]
+        [Display(Name = "Услуги")]
+        public string CategoryWork { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Значение {0} должно содержать не менее {2} символов.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        [Compare("Password", ErrorMessage = "Пароль и его подтверждение не совпадают.")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
