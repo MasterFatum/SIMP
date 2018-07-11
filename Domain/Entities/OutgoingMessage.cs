8aa7e5974a24998723b5be32ae6ed833028b6aa6
Pi Volt <master_Fatum@hotmail.com>
2018-07-11 17:13:51 +0300

    AddMessagesClasses

---
+++ Domain/Entities/OutgoingMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    class OutgoingMessage
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int UserToId { get; set; }
+        [Required(ErrorMessage = "Введите текст сообщения")]
+        public string Text { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
