7be387f9f6ba8848763e31528e9659e983ecb502
Pi Volt <master_Fatum@hotmail.com>
2018-07-11 17:07:53 +0300

    EditClassWorkerAndAddClassUser

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,46 @@ namespace Domain.Entities
 {
     class User
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введите фамилию")]
+        [Display(Name = "Фамилия")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Отчество")]
+        public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Введите адрес электронной почты")]
+        [Display(Name = "Адрес электронной почты")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Номер телефона")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "Фото профиля")]
+        public string PhotoProfile { get; set; }
+
+        [Required(ErrorMessage = "Введите имя пользователя")]
+        [Display(Name = "Имя пользователя")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
+        [StringLength(100, ErrorMessage = "Значение {0} должно содержать не менее {2} символов.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Подтверждение пароля")]
+        [Compare("Password", ErrorMessage = "Пароль и его подтверждение не совпадают.")]
+        public string ConfirmPassword { get; set; }
+
+        public DateTime CreateDate { get; set; }
 
     }
 }
--- Domain/Entities/Worker.cs
+++ Domain/Entities/Worker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,19 +12,18 @@ namespace Domain.Entities
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Введите фамилию")]
         [Display(Name = "Фамилия")]
         public string FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Введите имя")]
         [Display(Name = "Имя")]
         public string LastName { get; set; }
 
-        [Required]
         [Display(Name = "Отчество")]
         public string MiddleName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Введите адрес электронной почты")]
         [Display(Name = "Адрес электронной почты")]
         [EmailAddress]
         public string Email { get; set; }
@@ -31,19 +31,29 @@ namespace Domain.Entities
         [Display(Name = "Номер телефона")]
         public string PhoneNumber { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Выберите район")]
         [Display(Name = "Район")]
         public string Area { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Выберите сферу деятельности")]
         [Display(Name = "Сфера деятельности")]
         public string CategoryServises { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Выберите услугу")]
         [Display(Name = "Услуги")]
         public string CategoryWork { get; set; }
 
-        [Required]
+        [Display(Name = "Фото профиля")]
+        public string PhotoProfile { get; set; }
+
+        [Display(Name = "Фото примеров работ")]
+        public string PhotoWorks { get; set; }
+
+        [Required(ErrorMessage = "Введите имя пользователя")]
+        [Display(Name = "Имя пользователя" )]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
         [StringLength(100, ErrorMessage = "Значение {0} должно содержать не менее {2} символов.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Пароль")]
@@ -53,6 +63,8 @@ namespace Domain.Entities
         [Display(Name = "Подтверждение пароля")]
         [Compare("Password", ErrorMessage = "Пароль и его подтверждение не совпадают.")]
         public string ConfirmPassword { get; set; }
+
+        public DateTime CreateDate { get; set; }
 
     }
 }
